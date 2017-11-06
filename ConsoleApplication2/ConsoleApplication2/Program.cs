da58c24fc51c3229e33ea2af43b4b223475b7bfb
eee7ops8jgnub <eee7ops8jgnub@gmail.com>
2017-11-06 21:19:38 +0200

    //p

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -6,12 +6,105 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication2
 {
+    
+
     class Program
     {
+        public struct Song
+        {
+            public string Author;
+            public string Name;
+            public string Genre;
+            public double Lenght;
+        }
+
+
         static void Main(string[] args)
         {
-            //
-            Console.WriteLine();
+
+            Song[] song = new Song[5];
+            Song song; 
+                Song.Author = "authortest";
+                Song.Name = "nametest";
+                Song.Genre = "ganretest";
+                Song.Lenght = 03.23; 
+       
+                new song 
+                      Song.Author = "authortest";
+                      Song.Name = "nametest";
+                      Song.Genre = "ganretest";
+                      Song.Lenght = 02.12;
+                new song
+                          Song.Author = "authortest";
+                          Song.Name = "nametest";
+                          Song.Genre = "ganretest";
+                          Song.Lenght = 01.43; 
+                new song 
+                Song.Author = "authortest";
+                Song.Name = "nametest";
+                Song.Genre = "ganretest";
+                Song.Lenght = 05.43;
+                new song 
+                Song.Author = "authortest";
+                Song.Name = "nametest";
+                Song.Genre = "ganretest";
+                Song.Lenght = 02.24;
+            
+            // Song[] = new song[5];
+            //Song song1;
+            //song1.Author = "authortest";
+            //song1.Name = "nametest";
+            //song1.Genre = "ganretest";
+            //song1.Lenght = 03.23;
+            //Song song2;
+            //song2.Author = "authortest";
+            //song2.Name = "nametest";
+            //song2.Genre = "ganretest";
+            //song2.Lenght = 02.12;
+            //Song song3;
+            //song3.Author = "authortest";
+            //song3.Name = "nametest";
+            //song3.Genre = "ganretest";
+            //song3.Lenght = 01.43;
+            //Song song4;
+            //song4.Author = "authortest";
+            //song4.Name = "nametest";
+            //song4.Genre = "ganretest";
+            //song4.Lenght = 05.43;
+            //Song song5;
+            //song5.Author = "authortest";
+            //song5.Name = "nametest";
+            //song5.Genre = "ganretest";
+            //song5.Lenght = 02.24;
+
+
+            //Console.WriteLine("Author1 : " + song1.Author);
+            //Console.WriteLine("Neme1 : " + song1.Name);
+            //Console.WriteLine("Gendre1 : " + song1.Genre);
+            //Console.WriteLine("Lenght : " + song1.Lenght);
+            //Console.WriteLine("edit:");
+            //Console.Write("New Lenght : "); 
+            //song1.Lenght = double.Parse(Console.ReadLine());
+            //Console.WriteLine("new");
+            
+            //    Console.WriteLine("Author1 : " + song1.Author);
+            //    Console.WriteLine("Neme1 : " + song1.Name);
+            //    Console.WriteLine("Gendre1 : " + song1.Genre);
+            //    Console.WriteLine("Lenght : " + song1.Lenght);
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                if(>)
+                { Console.WriteLine(Song.Lenght); }
+            }
+
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
